85e72c1c409b73c2d7da34ee5bfeeef6c3003fac
krishnan <krishnan@tutecircle.com>
2019-09-11 02:05:05 +0530

    minor fix

--- landing_assets/js/service-worker.js
+++ landing_assets/js/service-worker.js
@@ -2,10 +2,21 @@ console.log("hey there");
 self.addEventListener('push', function(event) {
   let notificationOptions = {
     body : event.data.json().articleTitle,
-    icon : "../images/logo.png"
+    icon : "../images/logo.png",
+    data : {
+      url : event.data.json().articleUrl
+    }
   }
   self.registration.showNotification("Hive- It's Reading Time",notificationOptions);
 
   event.waitUntil(event);
 
+  });
+
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();  
+    console.log(JSON.stringify(event.notification));
+    clients.openWindow(event.notification.data.url)
+    event.waitUntil(event);
   });
